0c605fe6b3673fdd9c2a1a7aadc94040362721c4
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-01-19 14:31:46 +0100

    Add route for auction only on create and store

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuctionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
@@ -17,3 +18,4 @@ use App\Http\Controllers\CommentController;
 
 Route::resource('articles', ArticleController::class);
 Route::resource('articles.comments', CommentController::class)->only(['store']);
+Route::resource('articles.auctions', AuctionController::class)->only(['create', 'store']);
