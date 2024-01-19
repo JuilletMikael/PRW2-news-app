8ce803cf6fdb830a7f2c00e82760804212671f4d
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-01-19 14:58:51 +0100

    Chage home page text to same text but in french

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
 
 @section('content')
-    <h1>Most viewed articles</h1>
+    <h1>Articles les plus vus</h1>
     <ul>
         @foreach ($articles as $article)
-            <li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a> <i>views : {{$article->views_counter}}</i> </li>
+            <li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a> <i>vues : {{$article->views_counter}}</i> </li>
         @endforeach
     </ul>
 @endsection
