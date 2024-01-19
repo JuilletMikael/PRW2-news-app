6e6f48267a75bd5934c072f4e5939bc3b03d6f4c
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-01-19 14:10:53 +0100

    Adding article list of 5 most viewed articles by desc order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,6 +12,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //
+        return view('home', ['articles' => Article::orderby('views_counter', 'desc')->limit(5)->get()]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')
 
 @section('content')
-   Home
+    <h1>Most viewed articles</h1>
+    <ul>
+        @foreach ($articles as $article)
+            <li><a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a> <i>views : {{$article->views_counter}}</i> </li>
+        @endforeach
+    </ul>
 @endsection
