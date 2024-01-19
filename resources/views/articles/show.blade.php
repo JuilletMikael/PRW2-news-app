f5cdf76000e1c2571ce25d0271d9dec5f1df721f
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-01-19 15:04:01 +0100

    Add view check if auction exist

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -31,7 +31,9 @@
     @endforeach
     </ul>
 
-    Meilleur enchérissement le <strong>{{$auction->created_at}}</strong>
+    @if ($auction)
+        Meilleur enchérissement le <strong>{{$auction->created_at}}</strong>
+    @endif
     <button type="button" onclick="window.location='{{route('articles.auctions.create', $article)}}'">Enchérir sur l'article</button>
 
 @endsection
